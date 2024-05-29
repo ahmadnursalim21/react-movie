a93464b005e2703195b3070e8c56ed98dcf7f3e5
unknown <ahmadktb2323@gmailcom>
2024-05-29 18:54:10 +0700

    menambahkan sedikit di bagian footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
-
 const Footer = () => {
+  const listFooter = [
+    { link: "Instagram", path: "/" },
+    { link: "Twitter", path: "/hgh" },
+    { link: "Tiktok", path: "/jhjhj" },
+    { link: "WhatsApp", path: "/hjhj" },
+    { link: "Youtube", path: "/jhjkhjkh" },
+  ];
 
-    const listFooter =[
-        {link:"Instagram", path:"/"},
-        {link:"Twitter", path:"/"},
-        {link:"Tiktok", path:"/"},
-        {link:"WhatsApp", path:"/"},
-        {link:"Youtube", path:"/"},
-    ]
   return (
     <div>
-        <footer className='flex bg-slate-500'>
-            <div>
-                <div>
-                    <h1>This is Footer</h1>
-                </div>
-                <div>
-                    <ul>
-                        {listFooter.map(({link,path})=>{
-                            <a key={link} href={path}>{link}</a>
-                        })}
-                    </ul>
-                </div>
-            </div>
-        </footer>
+      <footer className="flex bg-slate-500">
+        <div>
+          <div>
+            <h1>This is Footer</h1>
+          </div>
+          <div>
+            <ul>
+              {listFooter.map(({ link, path }) => (
+                <a key={link} href={path}>
+                  {link}
+                </a>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
