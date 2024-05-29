6d344115ee3660335cc54025ea8ba99515ec2d90
ahmadnursalimktb <ahmadnursalimktb@gmail.com>
2024-05-29 11:36:31 +0000

    menambahkan file footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-
+import Footer from "./components/Footer"
 function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
